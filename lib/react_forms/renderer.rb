a57d82e36fe7ef9275604ca7d63c6afa43871dd9
ErrorSoul <neurofag@gmail.com>
2021-05-17 03:15:30 +0300

    add renderer

---
+++ lib/react_forms/renderer.rb
@@ -0,0 +1,35 @@
+require 'erb'
+module ReactForms
+
+  class Renderer
+
+
+   attr_reader :name, :names
+
+    def initialize
+      @name = name
+      @names = names
+    end
+
+
+    def names_string
+      names.join(", ")
+    end
+
+
+    def render
+      #binding.irb
+      ERB.new(File.read('templates/form.erb')).result(binding)
+    end
+
+    def render_to_file
+      File.open('my_file_erb.jsx', 'wb') do |f|
+        f.puts render
+      end
+
+    end
+
+
+  end
+
+end
