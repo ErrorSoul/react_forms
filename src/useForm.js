9dc2e73f545462a36569dad2aef17f41fe955340
ErrorSoul <neurofag@gmail.com>
2021-05-25 01:48:56 +0300

    update useForm

--- src/useForm.js
+++ src/useForm.js
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 const useForm = (names, submit, callback) => {
-  const preErrorData = Object.fromEntries(names.map((name) => [name, '']))
-  const [values, setValues] = useState(preErrorData)
-  const [errors, setErrors] = useState(preErrorData)
-  const [formValid, setFormValid] = useState(false)
+  const preErrorData = Object.fromEntries(names.map((name) => [name, ""]));
+  const [values, setValues] = useState(preErrorData);
+  const [errors, setErrors] = useState(preErrorData);
+  const [formValid, setFormValid] = useState(false);
 
   // const pre_validations = {
   //   email: {
@@ -20,94 +20,97 @@ const useForm = (names, submit, callback) => {
   const preValidationData = names.reduce(
     (acc, target) => ({ ...acc, [target]: { dirty: false, valid: false } }),
     {}
-  )
+  );
 
-  const [validations, setValidations] = useState(preValidationData)
+  const [validations, setValidations] = useState(preValidationData);
 
   const handleChange = (event) => {
-    const { name, value } = event.target
+    const { name, value } = event.target;
 
     setValues({
       ...values,
       [name]: value,
-    })
-    validateField(name, value)
-  }
+    });
+    validateField(name, value);
+  };
 
   const validateForm = () => {
-    setFormValid(Object.keys(values).every((elem) => validations[elem].valid))
-  }
+    setFormValid(Object.keys(values).every((elem) => validations[elem].valid));
+  };
 
   const validateField = (fieldName, value) => {
-    let errorsFields = errors
-    let validationFields = validations
-    let nameValidation = validationFields[fieldName]
-    let nameValid = nameValidation.valid
+    let errorsFields = errors;
+    let validationFields = validations;
+    let nameValidation = validationFields[fieldName];
+    let nameValid = nameValidation.valid;
 
-    nameValidation.dirty = true
+    nameValidation.dirty = true;
     //validationFields[fieldName].dirty = true
 
     switch (fieldName) {
-      case 'email':
-        nameValid = value.length > 0
-        errorsFields.email = nameValid ? '' : 'Неправильный формат email'
-        nameValidation.valid = nameValid
-        break
+      case "email":
+        nameValid = value.length > 0;
+        errorsFields.email = nameValid ? "" : "Неправильный формат email";
+        nameValidation.valid = nameValid;
+        break;
 
-      case 'login':
-        nameValid = value.length >= 3
-        errorsFields.login = nameValid ? '' : 'Неправильный формат login'
-        nameValidation.valid = nameValid
-        break
+      case "login":
+        nameValid = value.length >= 3;
+        errorsFields.login = nameValid ? "" : "Неправильный формат login";
+        nameValidation.valid = nameValid;
+        break;
 
-      case 'password':
-        nameValid = value.length >= 6
-        errorsFields.password = nameValid ? '' : 'Пароль слишком короткий'
-        nameValidation.valid = nameValid
-        break
+      case "password":
+        nameValid = value.length >= 6;
+        errorsFields.password = nameValid ? "" : "Пароль слишком короткий";
+        nameValidation.valid = nameValid;
+        break;
 
       default:
-        break
+        break;
     }
 
-    setErrors(errorsFields)
-    setValidations(validationFields)
-    validateForm()
-  }
+    setErrors(errorsFields);
+    setValidations(validationFields);
+    validateForm();
+  };
 
   useEffect(() => {
-    console.log('use Effect is started')
-  }, [values])
+    console.log("use Effect is started");
+  }, [values]);
 
   useEffect(() => {
-    console.log('Errors: ', errors)
-  }, [errors])
+    console.log("Errors: ", errors);
+  }, [errors]);
 
   const setInit = () => {
-    setErrors(preErrorData)
-    setValidations(preValidationData)
-    setValues(preErrorData)
-  }
+    setErrors(preErrorData);
+    setValidations(preValidationData);
+    setValues(preErrorData);
+  };
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-
-    const { answer, data } = submit(values)
-
-    setFormValid(false)
-
-    if (answer === 'error') {
-      for (let [key, value] of Object.entries(data)) {
-        validations[key].valid = false
-      }
-
-      setErrors(data)
-      setValidations(validations)
-    } else {
-      setInit()
-      callback()
+  const handleSubmit = async (e) => {
+    if (e && typeof e.preventDefault === "function") {
+      e.preventDefault();
     }
-  }
+    setFormValid(false);
+
+    let json;
+    try {
+      json = await submit(values);
+    } catch (e) {
+      const requestErrors = e.json.errors;
+      for (let [key, value] of Object.entries(requestErrors)) {
+        validations[key].valid = false;
+      }
+      setErrors(requestErrors);
+      setValidations(validations);
+      console.warn(e);
+      return;
+    }
+    setInit();
+    callback();
+  };
 
   return {
     handleChange,
@@ -116,7 +119,7 @@ const useForm = (names, submit, callback) => {
     errors,
     validations,
     formValid,
-  }
-}
+  };
+};
 
-export default useForm
+export default useForm;
